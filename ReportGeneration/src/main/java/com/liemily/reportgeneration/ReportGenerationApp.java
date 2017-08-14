f64ea6364e7f0fe8fc2bf369c05ddb1d92694432
emily-li <emily.li@live.co.uk>
2017-08-14 21:16:17 +0100

    Creating ReportGenerationApp

---
+++ ReportGeneration/src/main/java/com/liemily/reportgeneration/ReportGenerationApp.java
@@ -0,0 +1,14 @@
+package com.liemily.reportgeneration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Emily Li on 14/08/2017.
+ */
+@SpringBootApplication
+public class ReportGenerationApp {
+    public static void main(String[] args) {
+        SpringApplication.run(ReportGenerationApp.class, args);
+    }
+}
