5e870eecac503fa1fa86cfcbe6f81705d1b220c0
emily-li <emily.li@live.co.uk>
2017-08-14 21:26:41 +0100

    Adding initial template files for report generation

---
+++ ReportGeneration/src/main/java/com/liemily/reportgeneration/reports/StockReport.java
@@ -0,0 +1,7 @@
+package com.liemily.reportgeneration.reports;
+
+/**
+ * Created by Emily Li on 14/08/2017.
+ */
+public class StockReport extends Report {
+}
---
+++ ReportGeneration/src/test/java/com/liemily/reportgeneration/ReportInitialiserTest.java
@@ -0,0 +1,7 @@
+package com.liemily.reportgeneration;
+
+/**
+ * Created by Emily Li on 14/08/2017.
+ */
+public class ReportInitialiserTest {
+}
