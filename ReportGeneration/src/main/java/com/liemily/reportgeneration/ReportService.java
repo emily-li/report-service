64c61faaeb52daa1498457fbbeaf08fa66fdea50
emily-li <emily.li@live.co.uk>
2017-08-18 22:03:15 +0100

    Adding ReportService functionality

--- ReportGeneration/src/main/java/com/liemily/reportgeneration/ReportService.java
+++ ReportGeneration/src/main/java/com/liemily/reportgeneration/ReportService.java
@@ -1,7 +1,28 @@
 package com.liemily.reportgeneration;
 
+import com.liemily.reportgeneration.exceptions.ReportGenerationException;
+import com.liemily.reportgeneration.reports.Report;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Component;
+
+import java.nio.file.Path;
+
 /**
  * Created by Emily Li on 14/08/2017.
  */
+@Component
+@Lazy
 public class ReportService {
+    private ReportInitialiser reportInitialiser;
+
+    @Autowired
+    public ReportService(ReportInitialiser reportInitialiser) {
+        this.reportInitialiser = reportInitialiser;
+    }
+
+    public Path executeReport(ReportRequest reportRequest) throws ReportGenerationException {
+        Report report = reportInitialiser.getReport(reportRequest.getReportName());
+        return report.executeReport(reportRequest);
+    }
 }
--- ReportGeneration/src/test/java/com/liemily/reportgeneration/ReportServiceIT.java
+++ ReportGeneration/src/test/java/com/liemily/reportgeneration/ReportServiceIT.java
@@ -1,18 +1,29 @@
 package com.liemily.reportgeneration;
 
+import com.liemily.reportgeneration.reports.FILE_TYPE;
+import com.liemily.reportgeneration.reports.REPORT_NAME;
+import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.nio.file.Path;
 
 /**
  * Created by Emily Li on 14/08/2017.
  */
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class ReportServiceIT {
-    @Test
-    public void testRequestStockReportAsc() throws Exception {
-
-    }
+    @Autowired
+    private ReportService reportService;
 
     @Test
-    public void testRequestStockReportDesc() throws Exception {
-
+    public void testRequestReport() throws Exception {
+        ReportRequest reportRequest = new ReportRequest(REPORT_NAME.STOCK_REPORT, FILE_TYPE.XML);
+        Path reportPath = reportService.executeReport(reportRequest);
+        Assert.assertTrue(reportPath.toFile().exists());
     }
 }
