ce240661e50214e81082bf95242c431655872fa7
emily-li <emily.li@live.co.uk>
2017-08-18 21:39:57 +0100

    Adding Report-relevant exceptions

---
+++ ReportGeneration/src/main/java/com/liemily/reportgeneration/exceptions/ReportMarshallingException.java
@@ -0,0 +1,10 @@
+package com.liemily.reportgeneration.exceptions;
+
+/**
+ * Created by Emily Li on 18/08/2017.
+ */
+public class ReportMarshallingException extends Exception {
+    public ReportMarshallingException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
---
+++ ReportGeneration/src/main/java/com/liemily/reportgeneration/exceptions/ReportWritingException.java
@@ -0,0 +1,10 @@
+package com.liemily.reportgeneration.exceptions;
+
+/**
+ * Created by Emily Li on 18/08/2017.
+ */
+public class ReportWritingException extends Exception {
+    public ReportWritingException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
