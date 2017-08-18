14bc0bb7170d686ea24c511947883161dd744fb2
emily-li <emily.li@live.co.uk>
2017-08-18 12:44:53 +0100

    Deleting unneeded config

--- ReportGeneration/src/main/java/com/liemily/reportgeneration/ReportGenerationApp.java
+++ ReportGeneration/src/main/java/com/liemily/reportgeneration/ReportGenerationApp.java
@@ -1,15 +1,12 @@
 package com.liemily.reportgeneration;
 
-import com.liemily.reportgeneration.config.ReportGenerationConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Import;
 
 /**
  * Created by Emily Li on 14/08/2017.
  */
 @SpringBootApplication
-@Import(ReportGenerationConfig.class)
 public class ReportGenerationApp {
     public static void main(String[] args) {
         SpringApplication.run(ReportGenerationApp.class, args);
