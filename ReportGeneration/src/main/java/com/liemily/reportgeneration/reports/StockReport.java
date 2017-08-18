2e8b030f6386f620f78d597886e809cc50c600d3
emily-li <emily.li@live.co.uk>
2017-08-18 21:42:11 +0100

    Adding StockReport logic

--- ReportGeneration/src/main/java/com/liemily/reportgeneration/reports/StockReport.java
+++ ReportGeneration/src/main/java/com/liemily/reportgeneration/reports/StockReport.java
@@ -1,7 +1,18 @@
 package com.liemily.reportgeneration.reports;
 
+import com.liemily.reportgeneration.ReportRequest;
+import com.liemily.reportgeneration.reports.domain.ReportItem;
+import com.liemily.reportgeneration.reports.domain.ReportItems;
+import com.liemily.stock.Stock;
+import com.liemily.stock.StockService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Collection;
 
 /**
  * Created by Emily Li on 14/08/2017.
@@ -9,4 +20,29 @@ import org.springframework.stereotype.Component;
 @Component
 @Lazy
 public class StockReport extends Report {
+    private StockService stockService;
+
+    @Autowired
+    public StockReport(StockService stockService) {
+        this.stockService = stockService;
+    }
+
+    @Override
+    ReportItems generateReportItems(ReportRequest reportRequest) {
+        Collection<ReportItem> reportItems = new ArrayList<>();
+
+        Sort.Direction sortDirection = reportRequest.getSortDirection() == null ? Sort.DEFAULT_DIRECTION : reportRequest.getSortDirection();
+        Collection<Stock> stocks = getStocks(sortDirection);
+
+        for (Stock stock : stocks) {
+            ReportItem reportItem = new ReportItem(stock.getSymbol(), stock.getSymbol(), stock.getValue(), stock.getVolume(), new BigDecimal("0"));
+            reportItems.add(reportItem);
+        }
+        return new ReportItems(reportItems);
+    }
+
+    private Collection<Stock> getStocks(Sort.Direction direction) {
+        Sort sort = new Sort(direction, "value");
+        return stockService.getStocks(sort);
+    }
 }
--- ReportGeneration/src/test/java/com/liemily/reportgeneration/reports/StockReportTest.java
+++ ReportGeneration/src/test/java/com/liemily/reportgeneration/reports/StockReportTest.java
@@ -1,10 +1,26 @@
 package com.liemily.reportgeneration.reports;
 
+import com.liemily.reportgeneration.ReportRequest;
+import com.liemily.reportgeneration.reports.domain.ReportItems;
+import com.liemily.stock.Stock;
+import com.liemily.stock.StockRepository;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Sort;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.Unmarshaller;
+import java.math.BigDecimal;
+import java.nio.file.Path;
+import java.util.*;
+import java.util.stream.IntStream;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertTrue;
 
 /**
  * Created by Emily Li on 14/08/2017.
@@ -15,17 +31,58 @@ public class StockReportTest {
     @Autowired
     private StockReport stockReport;
 
+    @Autowired
+    private StockRepository stockRepository;
+
+    private Collection<Stock> repositoryStocks;
+
+    @Before
+    public void setup() {
+        repositoryStocks = new HashSet<>();
+        IntStream.range(0, 3).forEach(i -> repositoryStocks.add(new Stock("SYM" + i, new BigDecimal(i * i), 1)));
+        stockRepository.save(repositoryStocks);
+        repositoryStocks = stockRepository.findAll(); // Required to make the BigDecimal value consistent with the checks that retrieve stocks from the database later
+    }
+
     @Test
     public void testGetAllStockValues() throws Exception {
+        ReportRequest reportRequest = new ReportRequest(REPORT_NAME.STOCK_REPORT, FILE_TYPE.XML, Sort.DEFAULT_DIRECTION);
+        Path reportPath = stockReport.executeReport(reportRequest);
+
+        List<Stock> marshalledStocks = getStocksFromXML(reportPath);
+        assertTrue(marshalledStocks.containsAll(repositoryStocks));
     }
 
     @Test
-    public void testGetAllStockValuesAsc() throws Exception {
+    public void testGetAllStockValuesOrder() throws Exception {
+        final Sort.Direction[] DIRECTIONS = new Sort.Direction[]{Sort.Direction.ASC, Sort.Direction.DESC};
+        final Map<Sort.Direction, BigDecimal[]> EXPECTED_DIRECTION_VALUES = new HashMap<>();
 
+        TreeSet<BigDecimal> stockValues = new TreeSet<>();
+        repositoryStocks.forEach(stock -> stockValues.add(stock.getValue()));
+
+        EXPECTED_DIRECTION_VALUES.put(Sort.Direction.ASC, stockValues.toArray(new BigDecimal[repositoryStocks.size()]));
+        EXPECTED_DIRECTION_VALUES.put(Sort.Direction.DESC, stockValues.descendingSet().toArray(new BigDecimal[repositoryStocks.size()]));
+
+        for (Sort.Direction direction : DIRECTIONS) {
+            ReportRequest reportRequest = new ReportRequest(REPORT_NAME.STOCK_REPORT, FILE_TYPE.XML, direction);
+            Path reportPath = stockReport.executeReport(reportRequest);
+
+            List<Stock> marshalledStocks = getStocksFromXML(reportPath);
+            List<BigDecimal> marshalledStockValues = new ArrayList<>();
+            marshalledStocks.forEach(stock -> marshalledStockValues.add(stock.getValue()));
+
+            assertArrayEquals(EXPECTED_DIRECTION_VALUES.get(direction), marshalledStockValues.toArray(new BigDecimal[repositoryStocks.size()]));
+        }
     }
 
-    @Test
-    public void testGetAllStockValuesDesc() throws Exception {
+    private List<Stock> getStocksFromXML(Path xmlPath) throws Exception {
+        JAXBContext jaxbContext = JAXBContext.newInstance(ReportItems.class);
+        Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
+        ReportItems reportItems = (ReportItems) unmarshaller.unmarshal(xmlPath.toFile());
 
+        List<Stock> marshalledStocks = new ArrayList<>();
+        reportItems.getReportItemCollection().forEach(reportItem -> marshalledStocks.add(new Stock(reportItem.getStockSymbol(), reportItem.getValue(), reportItem.getVolume())));
+        return marshalledStocks;
     }
 }
