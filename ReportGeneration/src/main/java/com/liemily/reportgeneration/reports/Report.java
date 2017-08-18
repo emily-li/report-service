dc7c20c96cd3e2d192ca7d57261072ac289cf222
emily-li <emily.li@live.co.uk>
2017-08-18 21:37:42 +0100

    Adding file generation to report execution

--- ReportGeneration/src/main/java/com/liemily/reportgeneration/reports/Report.java
+++ ReportGeneration/src/main/java/com/liemily/reportgeneration/reports/Report.java
@@ -1,45 +1,80 @@
 package com.liemily.reportgeneration.reports;
 
 import com.liemily.reportgeneration.ReportRequest;
+import com.liemily.reportgeneration.exceptions.ReportMarshallingException;
+import com.liemily.reportgeneration.exceptions.ReportWritingException;
+import com.liemily.reportgeneration.reports.domain.ReportItems;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
 
-import java.util.LinkedHashMap;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import java.io.IOException;
+import java.io.StringWriter;
+import java.lang.invoke.MethodHandles;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.UUID;
 
 /**
  * Created by Emily Li on 14/08/2017.
  */
 public abstract class Report {
-    abstract LinkedHashMap<String, Object> getReportValues(ReportRequest reportRequest);
+    private static final Logger logger = LogManager.getLogger(MethodHandles.lookup().lookupClass());
 
-    public String executeReport(ReportRequest reportRequest) {
-        LinkedHashMap<String, Object> reportValues = getReportValues(reportRequest);
-        return getReportFile(reportRequest.getFileType(), reportValues);
+    abstract ReportItems generateReportItems(ReportRequest reportRequest);
+
+    public Path executeReport(ReportRequest reportRequest) throws ReportMarshallingException, ReportWritingException {
+        ReportItems reportItems = generateReportItems(reportRequest);
+        String reportContents = generateReport(reportItems, reportRequest.getFileType());
+
+        Path reportPath = generateReportPath(reportRequest.getFileType());
+        writeReport(reportContents, reportPath);
+        return reportPath;
     }
 
-    private String getReportFile(FILE_TYPE fileType, LinkedHashMap<String, Object> keyValues) {
-        String reportContents;
-        switch (fileType) {
-            case XML:
-                reportContents = getXML(keyValues);
-                break;
-            case CSV:
-                reportContents = getCSV(keyValues);
-                break;
-            default:
-                throw new UnsupportedOperationException("Unsupported report file type " + fileType);
+    private Path generateReportPath(FILE_TYPE fileType) {
+        Path path = Paths.get(UUID.randomUUID().toString() + "." + fileType.toString().toLowerCase());
+        path.toFile().deleteOnExit();
+        return path;
+    }
+
+    private void writeReport(String report, Path path) throws ReportWritingException {
+        try {
+            Files.write(path, report.getBytes());
+        } catch (IOException e) {
+            logger.info("Failed to write report to " + path.toAbsolutePath());
+            throw new ReportWritingException("Failed to write report to " + path.toAbsolutePath(), e);
         }
-        return reportContents;
+        logger.info("Wrote report to " + path.toAbsolutePath());
     }
 
-    private String getXML(LinkedHashMap<String, Object> keyValues) {
-        return null;
+    private String generateReport(ReportItems reportItems, FILE_TYPE fileType) throws ReportMarshallingException {
+        String contents;
+        if (fileType.equals(FILE_TYPE.XML)) {
+            contents = generateXML(reportItems);
+        } else {
+            throw new UnsupportedOperationException("Unsupported report file type " + fileType);
+        }
+        return contents;
     }
 
-    private String getCSV(LinkedHashMap<String, Object> keyValues) {
-        String xml = getXML(keyValues);
-        return convertXMLToCsv(xml);
-    }
+    private String generateXML(ReportItems reportItems) throws ReportMarshallingException {
+        try {
+            JAXBContext jaxbContext = JAXBContext.newInstance(ReportItems.class);
+            Marshaller marshaller = jaxbContext.createMarshaller();
+            marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
 
-    private String convertXMLToCsv(String xml) {
-        return null;
+            try (StringWriter stringWriter = new StringWriter()) {
+                marshaller.marshal(reportItems, stringWriter);
+                return stringWriter.toString();
+            }
+        } catch (JAXBException | IOException e) {
+            String msg = "Failed to marshal report to string";
+            logger.info(msg, e);
+            throw new ReportMarshallingException(msg, e);
+        }
     }
 }
