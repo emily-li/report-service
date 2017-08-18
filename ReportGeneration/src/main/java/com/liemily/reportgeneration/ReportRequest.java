6ba8a9c48fd3594513193d966aad522943e7f739
emily-li <emily.li@live.co.uk>
2017-08-18 21:40:32 +0100

    Adding sort order to ReportRequest (assumes default value given by concrete report)

--- ReportGeneration/src/main/java/com/liemily/reportgeneration/ReportRequest.java
+++ ReportGeneration/src/main/java/com/liemily/reportgeneration/ReportRequest.java
@@ -2,6 +2,7 @@ package com.liemily.reportgeneration;
 
 import com.liemily.reportgeneration.reports.FILE_TYPE;
 import com.liemily.reportgeneration.reports.REPORT_NAME;
+import org.springframework.data.domain.Sort;
 
 /**
  * Created by Emily Li on 18/08/2017.
@@ -9,10 +10,12 @@ import com.liemily.reportgeneration.reports.REPORT_NAME;
 public class ReportRequest {
     private final REPORT_NAME reportName;
     private final FILE_TYPE fileType;
+    private final Sort.Direction sortDirection;
 
-    public ReportRequest(REPORT_NAME reportName, FILE_TYPE fileType) {
+    public ReportRequest(REPORT_NAME reportName, FILE_TYPE fileType, Sort.Direction sortDirection) {
         this.reportName = reportName;
         this.fileType = fileType;
+        this.sortDirection = sortDirection;
     }
 
     public REPORT_NAME getReportName() {
@@ -21,5 +24,9 @@ public class ReportRequest {
 
     public FILE_TYPE getFileType() {
         return fileType;
+    }
+
+    public Sort.Direction getSortDirection() {
+        return sortDirection;
     }
 }
