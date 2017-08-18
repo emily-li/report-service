09fa6cc32783260d77bc477279cea3d4e6d7fdee
emily-li <emily.li@live.co.uk>
2017-08-18 21:43:09 +0100

    Adding Repository annotation to repository for clarity

--- Stock/src/main/java/com/liemily/stock/StockRepository.java
+++ Stock/src/main/java/com/liemily/stock/StockRepository.java
@@ -1,12 +1,16 @@
 package com.liemily.stock;
 
+import org.springframework.context.annotation.Lazy;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 /**
  * Created by Emily Li on 23/07/2017.
  */
+@Repository
+@Lazy
 public interface StockRepository extends JpaRepository<Stock, String> {
     @Modifying
     @Query("UPDATE Stock SET volume = volume - ?2 WHERE symbol = ?1 AND volume - ?2 >= 0")
