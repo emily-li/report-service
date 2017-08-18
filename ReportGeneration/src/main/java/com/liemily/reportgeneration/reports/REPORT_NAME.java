b090a9beb4eee42cbc76df825e1fd296bf0079f0
emily-li <emily.li@live.co.uk>
2017-08-18 16:39:59 +0100

    Adding REPORT_NAME enum to cover available reports for request

---
+++ ReportGeneration/src/main/java/com/liemily/reportgeneration/reports/REPORT_NAME.java
@@ -0,0 +1,19 @@
+package com.liemily.reportgeneration.reports;
+
+/**
+ * Created by Emily Li on 18/08/2017.
+ */
+public enum REPORT_NAME {
+    STOCK_REPORT;
+
+    @Override
+    public String toString() {
+        String[] splitName = super.toString().split("_");
+        StringBuilder name = new StringBuilder(splitName[0]);
+        for (int i = 1; i < splitName.length; i++) {
+            String firstLetter = Character.toString(splitName[i].charAt(0));
+            name.append(firstLetter.toUpperCase()).append(splitName[i].substring(1, splitName[i].length()));
+        }
+        return name.toString();
+    }
+}
