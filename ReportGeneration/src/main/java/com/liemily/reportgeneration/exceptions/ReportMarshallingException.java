6035b1748c312b274682cb4590f28d2fd401a907
emily-li <emily.li@live.co.uk>
2017-08-18 22:00:25 +0100

    Extending ReportGenerationException

--- ReportGeneration/src/main/java/com/liemily/reportgeneration/exceptions/ReportMarshallingException.java
+++ ReportGeneration/src/main/java/com/liemily/reportgeneration/exceptions/ReportMarshallingException.java
@@ -3,7 +3,7 @@ package com.liemily.reportgeneration.exceptions;
 /**
  * Created by Emily Li on 18/08/2017.
  */
-public class ReportMarshallingException extends Exception {
+public class ReportMarshallingException extends ReportGenerationException {
     public ReportMarshallingException(String message, Throwable cause) {
         super(message, cause);
     }
--- ReportGeneration/src/main/java/com/liemily/reportgeneration/exceptions/ReportWritingException.java
+++ ReportGeneration/src/main/java/com/liemily/reportgeneration/exceptions/ReportWritingException.java
@@ -3,7 +3,7 @@ package com.liemily.reportgeneration.exceptions;
 /**
  * Created by Emily Li on 18/08/2017.
  */
-public class ReportWritingException extends Exception {
+public class ReportWritingException extends ReportGenerationException {
     public ReportWritingException(String message, Throwable cause) {
         super(message, cause);
     }
