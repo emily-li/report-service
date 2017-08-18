b8f1c061cc1420e3e315ca105ef7de715fe4d6ed
emily-li <emily.li@live.co.uk>
2017-08-18 21:37:57 +0100

    Adding StockConfig import for repository access

--- ReportGeneration/src/main/java/com/liemily/reportgeneration/ReportGenerationApp.java
+++ ReportGeneration/src/main/java/com/liemily/reportgeneration/ReportGenerationApp.java
@@ -1,12 +1,15 @@
 package com.liemily.reportgeneration;
 
+import com.liemily.stock.config.StockConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 /**
  * Created by Emily Li on 14/08/2017.
  */
 @SpringBootApplication
+@Import(StockConfig.class)
 public class ReportGenerationApp {
     public static void main(String[] args) {
         SpringApplication.run(ReportGenerationApp.class, args);
