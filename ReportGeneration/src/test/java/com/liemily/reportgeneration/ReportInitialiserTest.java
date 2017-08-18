7f20da1a25b57914964385ccfd99a0bfe04a0dbf
emily-li <emily.li@live.co.uk>
2017-08-18 12:39:06 +0100

    Adding missing test runner

--- ReportGeneration/src/test/java/com/liemily/reportgeneration/ReportInitialiserTest.java
+++ ReportGeneration/src/test/java/com/liemily/reportgeneration/ReportInitialiserTest.java
@@ -3,12 +3,15 @@ package com.liemily.reportgeneration;
 import com.liemily.reportgeneration.reports.Report;
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 /**
  * Created by Emily Li on 14/08/2017.
  */
+@RunWith(SpringRunner.class)
 @SpringBootTest
 public class ReportInitialiserTest {
     @Autowired
